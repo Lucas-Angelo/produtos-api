fe0fc174ba9502ae0ced524b2dbafdcb93a72bce
lcs2001 <lcs2001_@hotmail.com>
2021-01-27 10:52:44 -0300

    Conexao com Postgres

--- src/main/java/com/lucasangelo/apirest/ApirestApplication.java
+++ src/main/java/com/lucasangelo/apirest/ApirestApplication.java
@@ -2,6 +2,7 @@ package com.lucasangelo.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ApirestApplication {
