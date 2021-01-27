6e6309a97f7d30a5aae7410c23b9bbbf21f60eee
lcs2001 <lcs2001_@hotmail.com>
2021-01-27 08:44:07 -0300

    Inicio do projeto, dependencias e CI

---
+++ src/test/java/com/lucasangelo/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasangelo.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
