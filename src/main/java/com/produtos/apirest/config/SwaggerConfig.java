1e1ab89f62b55743839149e51c1d47240dafbffe
lcs2001 <lcs2001_@hotmail.com>
2021-01-29 14:09:20 -0300

    Atualizacao

--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -37,8 +37,8 @@ public class SwaggerConfig {
                 "API REST de cadastro de produtos.",
                 "1.0",
                 "Terms of Service",
-                new Contact("Michelli Brito", "https://www.youtube.com/michellibrito",
-                        "michellidibrito@gmail.com"),
+                new Contact("Lucas Ângelo", "https://github.com/Lucas-Angelo",
+                        "lcs2001_@hotmail.com"),
                 "Apache License Version 2.0",
                 "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
         );
