cfbbd4cf22002b22b55f4502a48c93ddd0614a04
lcs2001 <lcs2001_@hotmail.com>
2021-01-29 09:05:18 -0300

    Correcao de bugs

--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -1,45 +1,48 @@
-package com.lucasangelo.apirest.config;
+package com.produtos.apirest.config;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 
 import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
-    // O Docket é uma classe adicionada no pom.xml e importada por aqui, para a aplicação
-    @Bean
-    public Docket produtoApi() {
+	
+	@Bean
+    public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.lucasangelo.apirest")) // Pacote onde está as classes Java
-                .paths(regex("/api.*")) // Qual caminho o swaggertype vai acessar
+                .apis(RequestHandlerSelectors.basePackage("com.produtos.apirest"))
+                .paths(regex("/api.*"))
                 .build()
-                .apiInfo(metaInfo()); // Método abaixo criado
+                .apiInfo(metaInfo());
     }
 
-    // Método contendo a info da API
     private ApiInfo metaInfo() {
+
         ApiInfo apiInfo = new ApiInfo(
                 "Produtos API REST",
-                "API REST de cadastro e consulta de produtos",
+                "API REST de cadastro de produtos.",
                 "1.0",
                 "Terms of Service",
-                new Contact("Lucas Ângelo", "https://github.com/Lucas-Angelo", "lcs2001_@hotmail.com"),
+                new Contact("Michelli Brito", "https://www.youtube.com/michellibrito",
+                        "michellidibrito@gmail.com"),
                 "Apache License Version 2.0",
-                "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
         );
+
         return apiInfo;
     }
 
